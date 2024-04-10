07d9bb5d86ce972957f8689d0f496e587ba3fd95
cleber-2021 <cleber.rocha2510@hotmail.com>
2024-04-10 03:16:25 -0300

    inicio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import GlobalStyle from "./styles/global";
+import styled from "styled-components";
+import { toast, ToastContainer } from "react-toastify";
+import form from "./components/From.js";
+import "react-toastify/dist/ReactToastify.css";
+
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 800px;
+  margin-top: 20px
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`;
+
+const Title = styled.h2``;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+   <>
+      <Container>
+        <Title>USUÁRIOS</Title>
+        <Form>
+
+        </Form>
+
+      </Container>
+      <ToastContainer autoClose={3000} position={POSITION.BOTTOM_LEFT}/>
+      <GlobalStyle/>
+   </>
   );
 }
 
